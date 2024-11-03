069e2fac7a3072ece599d6e76a4f2ff872e8135c
Rubenbros <rubenbros@hotmail.es>
2024-11-03 21:17:41 +0100

    Ejercicios para clase

--- src/ud1/ejercicios/ejercicio6/Main.java
+++ src/ud1/ejercicios/ejercicio6/Main.java
@@ -2,4 +2,7 @@ package ud1.ejercicios.ejercicio6;
 
 //Implementa un pequeño juego donde un hilo genera un número aleatorio y otro hilo pide al usuario que adivine ese número.
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
