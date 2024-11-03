15fefb72b3c619dafaf322ff52f112d0797f8a00
Rubenbros <rubenbros@hotmail.es>
2024-11-03 21:17:12 +0100

    Ejercicios para clase

--- src/ud1/ejercicios/ejercicio5/Main.java
+++ src/ud1/ejercicios/ejercicio5/Main.java
@@ -1,10 +1,9 @@
 package ud1.ejercicios.ejercicio5;
 
-/*Descripción: Crea dos hilos: uno que produce números aleatorios y otro que consume esos números.
-
-Utiliza un ArrayList o una cola para almacenar los números producidos.
-El productor agrega números a la lista y el consumidor los retira y los imprime.
-        Usa wait() y notify() para gestionar la sincronización.*/
+//Crea dos hilos: uno que produce números aleatorios y otro que consume esos números.
+//Utiliza un ArrayList o una cola para almacenar los números producidos.
 public class Main {
+    public static void main(String[] args) {
 
+    }
 }
