4e73970ba1023816035fa6e09b98eec02860eab5
Rubenbros <rubenbros@hotmail.es>
2024-11-11 12:27:22 +0100

    ej 7 y 8

---
+++ src/ud1/ejercicios/ejercicio7/Main.java
@@ -0,0 +1,10 @@
+package ud1.ejercicios.ejercicio7;
+
+//Simula el funcionamiento de una carrera con un semáforo
+//Los hilos de coches esperan el semáforo verde para avanzar y cuando han pasado esperan un tiempo aleatorio entre 1 y 5 segundos
+//para volver a pasar
+//El hilo del semáforo esta 3 segundos en verde donde los coches pueden pasar
+//El hilo del semáforo esta 2 segundos en rojo donde los coches no pueden pasar
+//El hilo del semáforo esta 1 segundo en naranja donde los coches tienen un 50% de posibilidades de pasar o quedarse esperando
+public class Main {
+}
