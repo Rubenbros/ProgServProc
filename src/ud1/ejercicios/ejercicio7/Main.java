cac2296c28ab164958c58a1327ef86a7b92accd5
rubenbros <ruben.jarne.cabanero@gmail.com>
2024-11-11 20:16:32 +0100

    solucion 7

--- src/ud1/ejercicios/ejercicio7/Main.java
+++ src/ud1/ejercicios/ejercicio7/Main.java
@@ -7,4 +7,25 @@ package ud1.ejercicios.ejercicio7;
 //El hilo del semáforo esta 2 segundos en rojo donde los coches no pueden pasar
 //El hilo del semáforo esta 1 segundo en naranja donde los coches tienen un 50% de posibilidades de pasar o quedarse esperando
 public class Main {
+
+    public static void main(String[] args) {
+        final int NUM_COCHES = 10;
+        Carretera carretera = new Carretera();
+        Semaforo semaforo = new Semaforo(carretera);
+        Coche[] coches = new Coche[NUM_COCHES];
+
+        semaforo.start();
+        for (int i = 0; i < coches.length; i++) {
+            coches[i] = new Coche(carretera, i);
+            coches[i].start();
+        }
+
+        try {
+            semaforo.join();
+            for (Coche coche : coches) coche.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
